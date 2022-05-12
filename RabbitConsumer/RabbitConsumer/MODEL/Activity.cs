a814b32033a9d73f558df8ff92d52756bfff64a7
Pero Peric <bruno.brckovic@gmail.com>
2022-05-12 03:07:04 +0200

    Finished

---
+++ RabbitConsumer/RabbitConsumer/MODEL/Activity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitConsumer.MODEL
+{
+    public class Activity
+    {
+        public int id { get; set; }
+        public string activity { get; set; }
+        public string type { get; set; }
+        public int participants { get; set; }
+        public float price { get; set; }
+        public string link { get; set; }
+        public string key { get; set; }
+        public double accessibility { get; set; }
+
+        public void ShowActivity(Activity activity)
+        {
+            Console.WriteLine(
+                "activity: " + activity.activity + Environment.NewLine +
+                "type: " + activity.type + Environment.NewLine +
+                "participants: " + activity.participants + Environment.NewLine +
+                "price: " + activity.price + Environment.NewLine +
+                "link: " + activity.link + Environment.NewLine +
+                "key: " + activity.key + Environment.NewLine +
+                "accessibility: " + activity.accessibility + Environment.NewLine
+                );
+        }
+    }
+}
---
+++ RabbitConsumer/RabbitConsumer/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Timers;
+using RabbitMQ.Client;
+using System.Text;
+using Newtonsoft.Json;
+using System.Web;
+using RabbitMQ.Client.Events;
+using RabbitConsumer.MODEL;
+
+namespace RabbitConsumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string rabbitmqconnection = $"amqp://{HttpUtility.UrlEncode("guest")}:{ HttpUtility.UrlEncode("guest")}@{ "localhost:5672"}";
+                var factory = new ConnectionFactory();
+                factory.Uri = new Uri(rabbitmqconnection);
+
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "inThing1",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, e) =>
+                    {
+                        var body = e.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        
+                        var act = System.Text.Json.JsonSerializer.Deserialize<Activity>(message);
+                        
+                        if(act.participants >= 1 && act.accessibility > 0.3)
+                        {
+                            act.ShowActivity(act);
+                        }
+
+
+                        //var body = e.Body.ToString();
+                        //var message = Encoding.UTF8.GetBytes((string)JsonConvert.DeserializeObject(body).ToString());
+
+                    };
+
+                    channel.BasicConsume("inThing1", true, consumer);
+
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Message not sent");
+            }
+        }
+    }
+}
